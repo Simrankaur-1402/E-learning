48d59b8e2e98c359a16db34278468e756237ed07
Simran Kaur <130176124+Simrankaur-1402@users.noreply.github.com>
2024-08-14 06:36:13 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// Navigation Menu Toggle for Mobile
+document.getElementById('login').addEventListener('click', function() {
+    alert('Login functionality coming soon!');
+});
+
+document.getElementById('signup').addEventListener('click', function() {
+    alert('Signup functionality coming soon!');
+});
+
+// Newsletter Subscription
+document.querySelector('.newsletter-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you for subscribing to our newsletter!');
+});
